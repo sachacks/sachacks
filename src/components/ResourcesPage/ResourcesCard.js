95034bf6de229006756ce01eb3a1a4cbe7b289e7
travischantravis <chancheukyin4463@gmail.com>
2020-08-24 17:50:22 -0700

    mobile version of resource page

--- src/components/ResourcesPage/ResourcesCard.js
+++ src/components/ResourcesPage/ResourcesCard.js
@@ -1,26 +1,35 @@
-import React, { useState } from 'react';
-import './ResourcesCard.css';
+import React, { useState } from "react";
+import "./ResourcesCard.css";
 
-import { GoTriangleDown } from 'react-icons/go';
+import { GoTriangleDown } from "react-icons/go";
 
-const ResourcesCard = ( props ) => {
-    const { topic, list } = props.content;
+const ResourcesCard = (props) => {
+  const { topic, list } = props.content;
 
-    const listContent = (
-        list.map( ( item, index ) => <li key={ index }>{ item }</li> )
-    );
+  const listContent = list.map((item, index) => <li key={index}>{item}</li>);
 
-    const [ open, setOpen ] = useState( { open: true } );
-    return (
-        <div className='vert-content'>
-            <div className='topic' onClick={ () => setOpen( !open ) }>
-                <h4>{ topic }</h4><GoTriangleDown fill='white' size='20px' style={ !open ? { marginLeft: '10px', transform: 'rotate(180deg)', transition: '0.5s' } : { marginLeft: '10px', transition: '0.5s' } } />
-            </div>
-            <ul style={ !open ? { visibility: 'visible', animation: 'slideDown 0.5s ease-in-out' } : { visibility: 'hidden' } }>
-                { listContent }
-            </ul>
-        </div>
-    );
+  const [open, setOpen] = useState(true);
+  return (
+    <div className="vert-content">
+      <div className="topic" onClick={() => setOpen(!open)}>
+        <h4>{topic}</h4>
+        <GoTriangleDown
+          fill="white"
+          size="20px"
+          style={
+            !open
+              ? {
+                  marginLeft: "10px",
+                  transform: "rotate(180deg)",
+                  transition: "0.5s",
+                }
+              : { marginLeft: "10px", transition: "0.5s" }
+          }
+        />
+      </div>
+      <ul className={!open ? "open-ul" : "close-ul"}>{listContent}</ul>
+    </div>
+  );
 };
 
 export default ResourcesCard;
