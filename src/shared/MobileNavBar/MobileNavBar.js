892639d163127d1a96b0988012fa9e2933482bf9
beam-ntth <beam4318.t@gmail.com>
2020-09-03 22:16:36 -0700

    touch up on mobile version

--- src/shared/MobileNavBar/MobileNavBar.js
+++ src/shared/MobileNavBar/MobileNavBar.js
@@ -26,14 +26,14 @@ const MobileNavBar = () => {
             </div>
 
             {/* Apply Icon */ }
-            <div className='mobile-hacker' style={ open ? { transform: 'translateY(-100px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(80px)', transition: '0.5s ease-in-out', opacity: '1' } }>
+            <div className='mobile-hacker' style={ open ? { transform: 'translateY(-100px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(100px)', transition: '0.5s ease-in-out', opacity: '1' } }>
                 <img src='/black-hacker-icon.png' alt='mobile hacker icon' /> <a href='https://sachacks.typeform.com/to/r2sYTJ' target='_blank' rel="noopener noreferrer">Team</a>
             </div>
             {/* <div className='mobile-mentor' style={ open ? { transform: 'translateY(-200px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(183px)', transition: '0.5s ease-in-out', opacity: '1' } }>
                 <img src='/black-mentor-icon.png' alt='mobile hacker icon' /> Mentor
             </div> */}
             {/* <div className='mobile-sponsor' style={ open ? { transform: 'translateY(-300px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(286px)', transition: '0.5s ease-in-out', opacity: '1' } }> */}
-            <div className='mobile-sponsor' style={ open ? { transform: 'translateY(-300px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(182px)', transition: '0.5s ease-in-out', opacity: '1' } }>
+            <div className='mobile-sponsor' style={ open ? { transform: 'translateY(-300px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(202px)', transition: '0.5s ease-in-out', opacity: '1' } }>
                 <img src='/black-sponsor-icon.png' alt='mobile hacker icon' /> Sponsor
             </div>
 
@@ -45,7 +45,7 @@ const MobileNavBar = () => {
                 <Link to='/resources'>Resources</Link>
             </div> */}
             {/* <div className='mobile-contact' style={ clicked ? { transform: 'translateY(-300px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(286px)', transition: '0.5s ease-in-out', opacity: '1' } }> */}
-            <div className='mobile-contact' style={ clicked ? { transform: 'translateY(-300px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(80px)', transition: '0.5s ease-in-out', opacity: '1' } }>
+            <div className='mobile-contact' style={ clicked ? { transform: 'translateY(-300px)', transition: '0.5s ease-in-out', opacity: '0' } : { transform: 'translateY(100px)', transition: '0.5s ease-in-out', opacity: '1' } }>
                 <a href='mailto: team@sachacks.io'>Contact</a>
             </div>
         </React.Fragment>
