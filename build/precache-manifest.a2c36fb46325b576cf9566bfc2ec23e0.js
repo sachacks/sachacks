a5c70dc7bf4f0c487de16603b03961022cd802e7
beam-ntth <beam4318.t@gmail.com>
2020-09-24 16:20:18 -0700

    add build

---
+++ build/precache-manifest.a2c36fb46325b576cf9566bfc2ec23e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27ce5c8f1cfb7fe0d255b11f67d7158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37904867ea53c4b64014",
+    "url": "/static/css/main.6f0417b9.chunk.css"
+  },
+  {
+    "revision": "ea0839b4479b01d2e1db",
+    "url": "/static/js/2.5ac0bc68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5ac0bc68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37904867ea53c4b64014",
+    "url": "/static/js/main.d3fcac39.chunk.js"
+  },
+  {
+    "revision": "cd1bd0b084828a4a9d8e",
+    "url": "/static/js/runtime-main.a8f90761.js"
+  }
+]);
