130c2a5d7be914e756bc276dbaed0ace3792bc87
beam-ntth <beam4318.t@gmail.com>
2020-09-24 14:03:43 -0700

    Fixed date

--- src/components/HeroPage/About.js
+++ src/components/HeroPage/About.js
@@ -13,7 +13,7 @@ const About = () => {
                 <span className='green'>Code.</span>
                 <span className='blue'>Launch.</span>
             </h2>
-            <h5 className='date'>FEBRUARY 19-21, 2021</h5>
+            <h5 className='date'>VIRTUALLY FEBRUARY 19-21, 2021</h5>
             <p className='description'>SacHacks is the first major intercollegiate hackathon in the Sacramento, California area. Our passion is to cultivate the untapped potential of those in Sac by launching their ideas into action during a 36-hour hackathon. </p>
             {/* <div className='map-buttons'>
                 <div className='map-button'>Google Maps</div>
